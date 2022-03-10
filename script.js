4c425102fe323610b5bfc450a788a2af8bc52ab3
Julien ichard WCS <julien.y.richard@gmail.com>
2022-03-10 11:54:27 +0100

    Mise en place des scripts de calcul en console.log sans controle usr

--- script.js
+++ script.js
@@ -1,7 +1,46 @@
+let val1 = 0;
+let val2 = 0;
+let operator = null;
+let num1 = true;
+
 const addValue = (num) => {
-  console.log(num)
+  if (num1) {
+    val1 = val1 + num;
+  } else {
+    val2 = val2 + num;
+  };
 }
 
+const addOperator = (signe) => {
+  console.log(signe)
+  operator = signe;
+  num1 = false;
+}
+
+const resetValue = () => {
+  val1 = 0;
+  val2 = 0;
+  operator = null;
+  num1 = true;
+}
 const calcul = () => {
-  console.log("Calcul")
+  console.log("Calcul");
+  switch (operator) {
+    case "+":
+      console.log(parseInt(val1) + parseInt(val2))
+      break;
+    case "-":
+      console.log(parseInt(val1) - parseInt(val2))
+      break;
+    case "*":
+      console.log(parseInt(val1) * parseInt(val2))
+      break;
+    case "/":
+      console.log(parseInt(val1) / parseInt(val2))
+      break;
+    default:
+      break;
+
+  }
+  resetValue();
 }
